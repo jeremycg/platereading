da0006cebc7d49c1dad40a73fe807b1a05a623ad
Jeremy Gray <jeremycgray@gmail.com>
2015-12-01 12:14:13 -0500

    trying to just plot mtcars

--- platereading/R/platereading.R
+++ platereading/R/platereading.R
@@ -283,15 +283,14 @@ plateshiny <- function(directory) {
         }
       )
       output$Plotstrain <- renderPlot({
-        z <- do.call(rbind, lapply(list.files()[grepl("[pP]late", list.files())], readonedir))
-        z$plate <- as.numeric(sub("[pP]late ", "", z$plate))
-        z$row <- as.numeric(as.character(z$row))
-        z <- left_join(z, strainlist, by = c(plate = "run", row = "column"))
-        toplot <- z[z$strain == input$straintoplot, ]
+        #z <- do.call(rbind, lapply(list.files()[grepl("[pP]late", list.files())], readonedir))
+        #z$plate <- as.numeric(sub("[pP]late ", "", z$plate))
+        #z$row <- as.numeric(as.character(z$row))
+        #z <- left_join(z, strainlist, by = c(plate = "run", row = "column"))
+        #toplot <- z[z$strain == input$straintoplot, ]
         plot(mtcars)
-        print(toplot)
-        s <- ggplot(toplot, aes(x = time, y = 1-od, col = factor(plate))) + geom_point() + facet_grid(~temperature) + geom_line(aes(group = well))
-        print(s)
+        #s <- ggplot(toplot, aes(x = time, y = 1-od, col = factor(plate))) + geom_point() + facet_grid(~temperature) + geom_line(aes(group = well))
+        #print(s)
         })
       observeEvent(input$do, {
         write.csv(namer(looper(getwd(), lag1 = input$initiallag, mumax1 = input$initialmumax, od01 = input$initialod0, odmax1 = input$initialodmax),"strainlist.csv"),file="outputfits.csv",row.names=F)
