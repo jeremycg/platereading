4332f9fa08efdb31ecc7d3465a81c7a8fca53e15
Jeremy Gray <jeremycgray@gmail.com>
2015-12-01 12:02:34 -0500

    trouble shooting new plot function

--- platereading/R/platereading.R
+++ platereading/R/platereading.R
@@ -287,7 +287,8 @@ plateshiny <- function(directory) {
         z$plate <- as.numeric(sub("[pP]late ", "", z$plate))
         z$row <- as.numeric(as.character(z$row))
         z <- left_join(z, strainlist, by = c(plate = "run", row = "column"))
-        toplot <- z[z$strain == Input$straintoplot, ]
+        toplot <- z[z$strain == input$straintoplot, ]
+        print(toplot)
         s <- ggplot(toplot, aes(x = time, y = 1-od, col = factor(plate))) + geom_point() + facet_grid(~temperature) + geom_line(aes(group = well))
         print(s)
         })
